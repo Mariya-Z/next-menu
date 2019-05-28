928f2067056bf9706ef6378ed1a5b688b2a61c0d
Mariya-Z <35897561+Mariya-Z@users.noreply.github.com>
2019-05-28 12:59:41 +0300

    add lib and prettierrc (#1)

---
+++ projects/next-menu/src/lib/next-menu-item/next-menu-item.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'next-menu-item',
+  templateUrl: './next-menu-item.component.html',
+  styleUrls: ['./next-menu-item.component.scss'],
+})
+export class NextMenuItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ projects/next-menu/src/lib/next-menu.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {NextMenuItemComponent} from './next-menu-item/next-menu-item.component';
+
+@NgModule({
+  declarations: [NextMenuItemComponent],
+  exports: [NextMenuItemComponent],
+})
+export class NextMenuModule {}
